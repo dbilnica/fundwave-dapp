737d659ed38a7a0562bcb7f4cffe230e81eb5fa3
dbilnica <david.bilnica1@gmail.com>
2024-01-23 16:39:42 +0100

    overflow is hidden

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,7 +17,7 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
           </Head>
 
           <ContextProvider>
-            <div className="flex flex-col h-screen">
+            <div className="flex flex-col h-screen no-overflow">
               <Notifications />
               <AppBar/>
               <ContentContainer>
