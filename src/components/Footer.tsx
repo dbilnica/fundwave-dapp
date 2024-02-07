227a9eb12bf286781d2b5fa02e99e19c4400e84f
dbilnica <david.bilnica1@gmail.com>
2024-02-07 11:30:59 +0100

    logo edit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,14 +5,14 @@ export const Footer: FC = () => {
     return (
         <div className="flex">
             <footer className="border-t-2 border-[#141414] bg-black hover:text-white w-screen" >
-                <div className="ml-12 py-12 mr-12">
+                <div className="ml-12 py-6 mr-12">
                     <div className="grid grid-cols-2 md:grid-cols-6 gap-2 md:gap-8 md:space-x-12 relative">
                         <div className='flex flex-col col-span-2 mx-4 items-center md:items-start'>
                             <div className='flex flex-row ml-1'>
                                 <Link href="https://solana.com" target="_blank" rel="noopener noreferrer" passHref className="text-secondary hover:text-white">
                                     <div className='flex flex-row ml-1'>
                                         <Image
-                                            src="/solanaLogo.png"
+                                            src="/fundwaveLogo.png"
                                             alt="solana icon"
                                             width={156}
                                             height={96}
@@ -20,8 +20,8 @@ export const Footer: FC = () => {
                                     </div>
                                 </Link>
                             </div>
-                            <div className="mb-6 m-1 sm:text-left place-items-start items-start font-normal tracking-tight text-secondary">
-                                        © 2023 Solana Template, edited by David Bilnica
+                            <div className="m-1 sm:text-left place-items-start items-start font-normal tracking-tight text-secondary">
+                                        © 2024 FundWave by David Bilnica
                             </div>
                         </div>
                     </div>
--- src/views/create/index.tsx
+++ src/views/create/index.tsx
@@ -7,10 +7,6 @@ export const CreateCampaignView: FC = ({ }) => {
   return (
     <div className="mx-auto p-4 max-w-7xl">
       <div className="md:hero-content flex flex-col">
-        <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mt-10 mb-8">
-          Solana Crowdfunding App
-        </h1>
-        {/* CONTENT GOES HERE */}
         <div className="text-center w-full">
           <Crowdfunding />
         </div>
