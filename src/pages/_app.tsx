37fe1fdf2971875004ab5746a95b1d753202bb37
dbilnica <david.bilnica1@gmail.com>
2024-02-11 21:29:18 +0100

    Vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,33 +1,35 @@
-import { AppProps } from 'next/app';
-import Head from 'next/head';
-import { FC } from 'react';
-import { ContextProvider } from '../contexts/ContextProvider';
-import { AppBar } from '../components/AppBar';
-import { ContentContainer } from '../components/ContentContainer';
-import { Footer } from '../components/Footer';
-import Notifications from '../components/Notification'
-require('@solana/wallet-adapter-react-ui/styles.css');
-require('../styles/globals.css');
+import { AppProps } from "next/app";
+import Head from "next/head";
+import { FC } from "react";
+import { ContextProvider } from "../contexts/ContextProvider";
+import { AppBar } from "../components/AppBar";
+import { ContentContainer } from "../components/ContentContainer";
+import { Footer } from "../components/Footer";
+import Notifications from "../components/Notification";
+import { Analytics } from "@vercel/analytics/react";
+
+require("@solana/wallet-adapter-react-ui/styles.css");
+require("../styles/globals.css");
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
-    return (
-        <>
-          <Head>
-            <title>FundWave</title>
-          </Head>
-
-          <ContextProvider>
-            <div className="flex flex-col h-screen no-overflow pt-20">
-              <Notifications />
-              <AppBar/>
-              <ContentContainer>
-                <Component {...pageProps} />
-                <Footer/>
-              </ContentContainer>
-            </div>
-          </ContextProvider>
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>FundWave</title>
+      </Head>
+      <ContextProvider>
+        <div className="flex flex-col h-screen no-overflow pt-20">
+          <Notifications />
+          <AppBar />
+          <ContentContainer>
+            <Component {...pageProps} />
+            <Footer />
+          </ContentContainer>
+        </div>
+      </ContextProvider>
+      <Analytics />
+    </>
+  );
 };
 
 export default App;
